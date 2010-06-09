140c86fcaff9790eea8a8c89c691a932cfc4e4d3
Yumi Nanako <yumileroy@yahoo.com>
2010-06-09 13:57:41 +0800

    Same as previous commit, except previous commit was for css. This now is for js.

--- js/js.php
+++ js/js.php
@@ -6,7 +6,15 @@ $now = time();
 $then = gmstrftime("%a, %d %b %Y %H:%M:%S GMT", $now + 365 * 86440);
 header("Expires: $then");
 
-ob_start("ob_gzhandler");
+ob_start("compress");
+
+function compress($buffer) {
+    /* remove comments */
+    $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+    /* remove tabs, spaces, newlines, etc. */
+    $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
+    return $buffer;
+}
 
 /* your js files */
 include('scriptaculous/lib/prototype.js');
