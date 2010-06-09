05a27e441cbb49fb5e96a30b56ee3a60df0da68c
Yumi Nanako <yumileroy@yahoo.com>
2010-06-09 14:06:29 +0800

    Undid the previous js compression.

--- js/js.php
+++ js/js.php
@@ -6,15 +6,7 @@ $now = time();
 $then = gmstrftime("%a, %d %b %Y %H:%M:%S GMT", $now + 365 * 86440);
 header("Expires: $then");
 
-ob_start("compress");
-
-function compress($buffer) {
-    /* remove comments */
-    $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
-    /* remove tabs, spaces, newlines, etc. */
-    $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
-    return $buffer;
-}
+ob_start("ob_gzhandler");
 
 /* your js files */
 include('scriptaculous/lib/prototype.js');
